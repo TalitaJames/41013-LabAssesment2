7605112e5b408f54da8271351e663277d0b81258
Mikhail Karpov <mikhail.karpov@student.uts.edu.au>
2024-10-27 15:31:02 +1100

    Spherical joint for gantry

--- @Gantry/Gantry.m
+++ @Gantry/Gantry.m
@@ -24,6 +24,9 @@ classdef Gantry < RobotBaseClass
             link(1) = Link([0      0           0        pi/2    1]);
             link(2) = Link([-pi/2  0           0       -pi/2    1]);
             link(3) = Link([0      0           0        0       1]);
+            link(4) = Link([0      0.1         0        pi/2    0]);
+            link(5) = Link([0      0           0        pi/2    0]);
+            link(6) = Link([0      0           0       -pi/2    0]);
 
             link(1).qlim = [0.01 0.6];
             link(2).qlim = [0.01 1.3];
